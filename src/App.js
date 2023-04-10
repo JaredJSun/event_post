f9541d44a9b83e77a1129ff03f710c7fb3ce34ab
JaredJSun <jiajun_sun@tamu.edu>
2023-04-09 20:20:20 -0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import EventForm from './components/EventForm';
+import EventList from './components/EventList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Create Event</h1>
+      <EventForm />
+      <h2>Event List</h2>
+      <EventList />
     </div>
   );
 }
