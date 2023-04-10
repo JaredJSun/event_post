2adea0319584952c8f441007d99431238c0f9afa
JaredJSun <jiajun_sun@tamu.edu>
2023-04-09 20:14:05 -0500

    app.js

---
+++ server/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// 连接到MongoDB数据库
+mongoose.connect('mongodb://localhost/events', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// 创建Event模型
+const eventSchema = new mongoose.Schema({
+  name: String,
+  location: String,
+  description: String,
+});
+
+const Event = mongoose.model('Event', eventSchema);
+
+// API路由
+app.get('/api/events', async (req, res) => {
+  const events = await Event.find();
+  res.json(events);
+});
+
+app.post('/api/events', async (req, res) => {
+  const event = new Event(req.body);
+  await event.save();
+  res.json(event);
+});
+
+// 监听端口
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
