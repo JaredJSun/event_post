b37c09cccf2507873453a5a70a50303399653eaf
JaredJSun <jiajun_sun@tamu.edu>
2023-04-13 18:11:55 -0500

    add showMap

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,20 +1,36 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import '../styles/EventForm.css'; // 导入CSS文件
+import { getGeocode, getLatLng } from "use-places-autocomplete"; // import google maps api
 
 const EventForm = () => {
   const [name, setName] = useState('');
   const [location, setLocation] = useState('');
   const [description, setDescription] = useState('');
 
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const event = { name, location, description };
+  //   await axios.post('http://localhost:3001/api/events', event);
+  //   setName('');
+  //   setLocation('');
+  //   setDescription('');
+  // };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const event = { name, location, description };
-    await axios.post('http://localhost:3001/api/events', event);
-    setName('');
-    setLocation('');
-    setDescription('');
-  };
+    try {
+      const results = await getGeocode({ address: location });
+      const { lat, lng } = await getLatLng(results[0]);
+      const event = { name, location, description, lat, lng };
+      await axios.post('http://localhost:3001/api/events', event);
+      setName('');
+      setLocation('');
+      setDescription('');
+    } catch (error) {
+      console.error("Error fetching geocode data:", error);
+    }
+  };  
 
   return (
     <>
--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../styles/EventList.css'; // 导入CSS文件
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import Geocode from 'react-geocode';
+
+Geocode.setApiKey(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
 
 const EventList = () => {
   const [events, setEvents] = useState([]);
+  const [mapLocation, setMapLocation] = useState(null);
+  const [showMap, setShowMap] = useState(false);
 
   useEffect(() => {
     const fetchEvents = async () => {
@@ -13,6 +19,17 @@ const EventList = () => {
     fetchEvents();
   }, []);
 
+  const handleShowMap = async (location) => {
+    const response = await Geocode.fromAddress(location);
+    const { lat, lng } = response.results[0].geometry.location;
+    setMapLocation({ lat, lng });
+    setShowMap(true);
+  };
+
+  const handleCloseMap = () => {
+    setShowMap(false);
+  };
+
   return (
     <>
       <h2 style={{ textAlign: 'center' }}>Event List</h2>
@@ -20,13 +37,69 @@ const EventList = () => {
         {events.map((event) => (
           <div className="event-item" key={event._id}>
             <h3>{event.name}</h3>
-            <p>Location: {event.location}</p>
+            <p>
+              Location: {event.location}{' '}
+              <button onClick={() => handleShowMap(event.location)}>
+                Show Map
+              </button>
+            </p>
             <p>Description: {event.description}</p>
           </div>
         ))}
       </div>
+      {showMap && mapLocation && (
+        <div>
+          <button onClick={handleCloseMap}>Close Map</button>
+          <MapContainer
+            center={mapLocation}
+            zoom={13}
+            style={{ height: '400px', width: '100%' }}
+          >
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker position={mapLocation}>
+              <Popup>Event Location</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      )}
     </>
   );
 };
 
 export default EventList;
+
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import '../styles/EventList.css'; // 导入CSS文件
+
+// const EventList = () => {
+//   const [events, setEvents] = useState([]);
+
+//   useEffect(() => {
+//     const fetchEvents = async () => {
+//       const response = await axios.get('http://localhost:3001/api/events');
+//       setEvents(response.data);
+//     };
+//     fetchEvents();
+//   }, []);
+
+//   return (
+//     <>
+//       <h2 style={{ textAlign: 'center' }}>Event List</h2>
+//       <div className="event-list">
+//         {events.map((event) => (
+//           <div className="event-item" key={event._id}>
+//             <h3>{event.name}</h3>
+//             <p>Location: {event.location}</p>
+//             <p>Description: {event.description}</p>
+//           </div>
+//         ))}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default EventList;
